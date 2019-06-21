f527c82e0f4fa0df5d01bd0b5e6e390126fc75c3
xiao <52027916+SqrtThree-xiao@users.noreply.github.com>
2019-06-22 00:21:26 +0800

    01.LuaFramework 初步搭建

---
+++ Assets/01.LuaFramework/Edirtor/LuaTagEdirtor.cs
@@ -0,0 +1,7 @@
+﻿using UnityEditor;
+
+public class LuaTagEdirtor : Editor {
+
+	//TODO 加载.lua文件资源
+
+}
---
+++ Assets/01.LuaFramework/LuaEnvManager.cs
@@ -0,0 +1,27 @@
+﻿//xlua管理类
+using XLua;
+
+public class LuaEvnManager
+{
+	private LuaEvnManager(){}
+	private static LuaEnv _L;
+	private static LuaEvnManager _instance;
+	public static LuaEvnManager Instance 
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = new LuaEvnManager();
+				_L = new LuaEnv();
+			}
+			return _instance;
+		}
+	}
+
+	public void RunLuaCode(string msg)
+	{
+		_L.DoString(msg);
+		_L.Dispose();
+	}
+}
