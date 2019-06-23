9186571b8d42b44fa55189373e9dd98218fbb212
Akatsuki_Shio <1009298085@qq.com>
2019-06-23 15:46:28 +0900

    上传摄像机测试场景CameraTest以及他的脚本、预制体、材质等文件

---
+++ Assets/Script/CameraTest/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    CharacterController playerCC;//角色控制器
+    [SerializeField]
+    private float speed;
+
+	void Start () {
+        playerCC = transform.GetComponent<CharacterController>();//初始化
+	}
+	
+	
+	void Update () {
+        Move();
+	}
+    void Move()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            playerCC.SimpleMove(new Vector3(0, 0, transform.position.z*speed));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerCC.SimpleMove(new Vector3(0, 0, -transform.position.z * speed));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerCC.SimpleMove(new Vector3(transform.position.x * speed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerCC.SimpleMove(new Vector3(-transform.position.x * speed, 0, 0));
+        }
+
+
+
+
+
+    }
+
+
+
+}
---
+++ Assets/Script/CameraTest/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    public Transform tips;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.N))
+        {
+            tips.gameObject.SetActive(!tips.gameObject.activeInHierarchy);
+        }
+	}
+}
