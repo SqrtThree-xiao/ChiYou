176a7f34c0c766de33b37e9fc5839856359a33a5
SqrtThree-xiao <52027916+SqrtThree-xiao@users.noreply.github.com>
2019-06-26 14:09:58 +0800

    改善Unity中lua文件环境 1，编辑情况下课查看Lua文件内容 2，可识别lua内容

---
+++ Assets/01.LuaFramework/SGTextAsset.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;  
+using System.IO;  
+
+[System.Serializable]  
+public class SGTextAsset  
+{  
+    public Object textAsset;  
+      
+    private string text = string.Empty;  
+    private TextAsset asset = null;  
+    public string Text  
+    {  
+        get  
+        {  
+            if (textAsset is DefaultAsset)  
+            {  
+                if (string.IsNullOrEmpty(text))  
+                {  
+                    text = File.ReadAllText(AssetDatabase.GetAssetPath(textAsset));  
+                }  
+                return text;  
+            }  
+            else if (textAsset is TextAsset)  
+            {  
+                if (asset == null)  
+                {  
+                    asset = textAsset as TextAsset;  
+                }  
+                return asset.text;  
+            }  
+            else  
+            {  
+                return null;  
+            }  
+        }  
+    }  
+}  
