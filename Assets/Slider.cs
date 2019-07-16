8170930111061fc7683c45c7c47203e7b386efc1
SqrtThree-xiao <52027916+SqrtThree-xiao@users.noreply.github.com>
2019-07-16 16:10:48 +0800

    建立MyCode,用于算法学习

---
+++ Assets/Slider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour {
+
+    Vector2 pos;
+	// Use this for initialization
+	void Start () {
+        pos = gameObject.GetComponent<RectTransform>().position;
+
+        RectTransform rect = gameObject.GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //pos = new Vector2(pos.x + 15f * Time.deltaTime, pos.y);
+        //gameObject.GetComponent<RectTransform>().position = pos;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("开始：" + collision.transform.name);
+        var Vect3 = collision.transform.localScale;
+        Vect3 += new Vector3(0.5f, 0.5f, 0.5f);
+        collision.transform.localScale = Vector3.Lerp(collision.transform.localScale, Vect3, 0.3f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("结束：" + collision.transform.name);
+        var Vect3 = collision.transform.localScale;
+        Vect3 -= new Vector3(0.5f, 0.5f, 0.5f);
+        collision.transform.localScale = Vector3.Lerp(collision.transform.localScale, Vect3, 0.3f);
+    }
+}
---
+++ LeetCode/MyCode/MyCode/Case/program001.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * 题目描述
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * 示例：
+ * 给定 nums = [2, 7, 11, 15], taget = 9
+ * 因为 nums[0] + num[1] = taget
+ * 所以返回 [0, 1]
+ */
+
+namespace MyCode.Case
+{
+    class program001
+    {
+
+    }
+}
