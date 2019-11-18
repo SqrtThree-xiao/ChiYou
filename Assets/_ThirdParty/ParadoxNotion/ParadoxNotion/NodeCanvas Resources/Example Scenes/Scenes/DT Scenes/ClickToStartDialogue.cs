6a644302c1ab3ba4e43784b1bac172843c373e15
xiao <52027916+SqrtThree-xiao@users.noreply.github.com>
2019-11-18 15:47:36 +0800

    添加CanvasNode官方案例

---
+++ Assets/_ThirdParty/ParadoxNotion/ParadoxNotion/NodeCanvas Resources/Example Scenes/Scenes/DT Scenes/ClickToStartDialogue.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using NodeCanvas.DialogueTrees;
+
+public class ClickToStartDialogue : MonoBehaviour {
+
+	public DialogueTreeController dialogueController;
+
+	void OnMouseDown(){
+		gameObject.SetActive(false);
+		dialogueController.StartDialogue( OnDialogueEnd );
+	}
+
+	void OnDialogueEnd(bool success){
+		gameObject.SetActive(true);
+	}
+}
