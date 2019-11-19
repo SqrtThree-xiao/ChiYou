5b8136bcce5a1bbdba051c2249f4ff127c48eaf1
xiao <52027916+SqrtThree-xiao@users.noreply.github.com>
2019-11-19 19:43:49 +0800

    测试行为树例子

---
+++ Assets/xiao_AI_Block/Code/Snake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace xiao_Demo01
+{
+    public class Snake : MonoBehaviour//, IEnumerable
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        public static KeyCode Listener_keyboard()
+        {
+            KeyCode keyCode = 0;
+
+            if (EventType.KeyDown == Event.current.type)
+            {
+                keyCode = Event.current.keyCode;
+            }
+
+            return keyCode;
+        }
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+    }
+}
+
+
